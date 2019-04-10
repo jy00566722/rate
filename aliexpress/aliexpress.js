52c493dcfccd7f6e123b3f5757164c4a398e2221
pengfeng <jy00566723@gmail.com>
2019-04-10 21:40:33 +0800

    fi

---
+++ aliexpress/aliexpress.js
@@ -0,0 +1,41 @@
+//首页
+<span class="current-price" data-spm-anchor-id="2114.11010108.02001.i0.7ee2649bZXdEoB">US $5.86</span>
+<span class="current-price" data-spm-anchor-id="2114.11010108.06001.i1.7ee2649bZXdEoB">US $74.99</span>
+
+//详情页主价
+<span class="p-symbol" itemprop="priceCurrency" content="USD" data-spm-anchor-id="2114.10010108.1000016.i2.7c358970tXDV7d">US $</span>
+<span id="j-sku-price" class="p-price" itemprop="price" data-spm-anchor-id="2114.10010108.1000016.i1.7c358970tXDV7d">1.50</span>
+
+<span class="p-symbol" itemprop="priceCurrency" content="USD" data-spm-anchor-id="2114.10010108.1000016.i1.131d430dPVhW7t">US $</span>
+<span id="j-sku-discount-price" class="p-price"><span itemprop="lowPrice">5.99</span> - <span itemprop="highPrice" data-spm-anchor-id="2114.10010108.1000016.i0.131d430dPVhW7t">15.96</span></span>
+
+
+//详情页中间关联产品
+<em style="color: #bd1a1d;font-style: normal;font-weight: 700;" data-spm-anchor-id="2114.10010108.1000023.i0.7c358970tXDV7d">USD 3.99</em>
+<em style="color: #bd1a1d;font-style: normal;font-weight: 700;" data-spm-anchor-id="2114.10010108.1000023.i3.7c358970tXDV7d">USD 2.44</em>
+
+//详情页左侧产品
+<b class="notranslate" data-spm-anchor-id="2114.10010108.100009.i1.7c358970tXDV7d">US $2.29</b>
+<b class="notranslate" data-spm-anchor-id="2114.10010108.100009.i1.131d430dPVhW7t">US $5.34</b>
+<b class="notranslate" data-spm-anchor-id="2114.10010108.1000010.i0.131d430dPVhW7t">US $22.41</b>
+
+//详情页下面产品
+<b class="notranslate" data-spm-anchor-id="2114.10010108.1000013.i1.7c358970tXDV7d">US $1.98</b>
+<b class="notranslate" data-spm-anchor-id="2114.10010108.1000015.i2.7c358970tXDV7d">US $2.39</b>
+
+//类目
+<span class="value" itemprop="price" data-spm-anchor-id="2114.search0203.3.i0.6c8e5504Z2axgO">US $42.27</span>
+<span class="value" itemprop="price" data-spm-anchor-id="2114.search0203.3.i2.6c8e5504Z2axgO">US $3.59 - 3.77</span>
+<span class="value" itemprop="price" data-spm-anchor-id="2114.search0203.3.i3.6c8e5504Z2axgO">US $9.00</span>
+
+//类目下面
+<p class="p4p-price-list" data-spm-anchor-id="2114.search0203.6.i0.6c8e5504Z2axgO">US $49.20<span class="fwn"> / piece</span></p>
+
+//搜索
+<span class="value" itemprop="price" data-spm-anchor-id="2114.search0204.3.i1.5c0432ddBqUncr">US $4.80 - 7.60</span>
+<span class="value" itemprop="price" data-spm-anchor-id="2114.search0204.3.i3.5c0432ddBqUncr">US $8.39 - 21.69</span>
+
+//搜索下面
+<p class="p4p-price-list" data-spm-anchor-id="2114.search0204.6.i1.5c0432ddBqUncr">US $7.76 - 13.49<span class="fwn" data-spm-anchor-id="2114.search0204.6.i0.5c0432ddBqUncr"> / piece</span></p>
+<p class="p4p-price-list" data-spm-anchor-id="2114.search0204.6.i3.5c0432ddBqUncr">US $32<span class="fwn" data-spm-anchor-id="2114.search0204.6.i2.5c0432ddBqUncr"> / piece</span></p>
+
